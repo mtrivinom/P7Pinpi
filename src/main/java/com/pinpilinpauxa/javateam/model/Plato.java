a5a0e0de579e32e9ea04bc169d67a2bae468dfe0
MAlmendros <101110582+MAlmendros@users.noreply.github.com>
2022-11-09 21:10:35 +0100

    add Plato model

---
+++ src/main/java/com/pinpilinpauxa/javateam/model/Plato.java
@@ -0,0 +1,71 @@
+package com.pinpilinpauxa.javateam.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "plato")
+public class Plato {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private int id_p;
+
+    private String nombre_p;
+    private String descripcion;
+    private int kcal;
+    private int unidades;
+    private String nombre_t;
+
+    public int getId_p() {
+        return id_p;
+    }
+
+    public void setId_p(int id_p) {
+        this.id_p = id_p;
+    }
+
+    public String getNombre_p() {
+        return nombre_p;
+    }
+
+    public void setNombre_p(String nombre_p) {
+        this.nombre_p = nombre_p;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getKcal() {
+        return kcal;
+    }
+
+    public void setKcal(int kcal) {
+        this.kcal = kcal;
+    }
+
+    public int getUnidades() {
+        return unidades;
+    }
+
+    public void setUnidades(int unidades) {
+        this.unidades = unidades;
+    }
+
+    public String getNombre_t() {
+        return nombre_t;
+    }
+
+    public void setNombre_t(String nombre_t) {
+        this.nombre_t = nombre_t;
+    }
+
+}
