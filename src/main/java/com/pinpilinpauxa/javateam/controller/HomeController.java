b501f05a292074912dc64b5e8a62c9da39913377
MAlmendros <101110582+MAlmendros@users.noreply.github.com>
2022-11-14 20:11:25 +0100

    feat: rename home controller and clients menu name

--- src/main/java/com/pinpilinpauxa/javateam/controller/HomeController.java
+++ src/main/java/com/pinpilinpauxa/javateam/controller/HomeController.java
@@ -5,16 +5,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class FirstController {
+public class HomeController {
 
     @GetMapping
     public String greeting() {
-
         return "Hello World from Spring Boot with Pinpilinpauxa";
     }
+
     @RequestMapping("/goodbye")
     public String goodbye(){
-
         return "Goodbye! See you in the next product :)";
     }
 }
