6287c16f53013abfcfff25c482e4a14ea77e93ea
MAlmendros <101110582+MAlmendros@users.noreply.github.com>
2022-11-14 20:35:52 +0100

    feat: rework index, header and footer

--- src/main/java/com/pinpilinpauxa/javateam/controller/HomeController.java
+++ src/main/java/com/pinpilinpauxa/javateam/controller/HomeController.java
@@ -1,19 +1,12 @@
 package com.pinpilinpauxa.javateam.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
-
-    @GetMapping
-    public String greeting() {
-        return "Hello World from Spring Boot with Pinpilinpauxa";
-    }
-
-    @RequestMapping("/goodbye")
-    public String goodbye(){
-        return "Goodbye! See you in the next product :)";
+    @GetMapping("/")
+    String index() {
+        return "index";
     }
 }
