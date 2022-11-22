59446ec2644addb871e07063406db71cf20d07a7
MAlmendros <101110582+MAlmendros@users.noreply.github.com>
2022-11-22 22:55:04 +0100

    feat: register form and login with database

---
+++ src/main/java/com/pinpilinpauxa/javateam/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.pinpilinpauxa.javateam.repository;
+
+import com.pinpilinpauxa.javateam.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
+    public User findByEmail(String email);
+}
