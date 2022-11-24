09cdb01edfc817f294ff0e4aa7af83bced06c708
joseanuoc <101591617+joseanuoc@users.noreply.github.com>
2022-11-24 15:48:30 +0100

    Update User.java
    Cambio "last name" por "usuario"

--- src/main/java/com/pinpilinpauxa/javateam/model/User.java
+++ src/main/java/com/pinpilinpauxa/javateam/model/User.java
@@ -13,18 +13,19 @@ public class User {
     @Column(nullable = false, unique = true, length = 45)
     private String email;
 
-    @Column(nullable = false, length = 64)
+    @Column(name = "password", nullable = false, length = 64)
     private String password;
 
-    @Column(name = "first_name", nullable = false, length = 20)
-    private String firstName;
+    @Column(name = "nombre", nullable = false, length = 20)
+    private String nombre;
 
-    @Column(name = "last_name", nullable = false, length = 20)
-    private String lastName;
+    @Column(name = "usuario", nullable = false, length = 20)
+    private String usuario;
 
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
@@ -32,6 +33,7 @@ public class User {
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
@@ -39,21 +41,47 @@ public class User {
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public String getNombre() {
+        return nombre;
     }
 
-    public String getLastName() {
-        return lastName;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+
+    public String getUsuario() {
+        return usuario;
     }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+
+    public User(Long id, String email, String password, String nombre, String usuario) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.nombre = nombre;
+        this.usuario = usuario;
+    }
+
+    public User(String email, String password, String nombre, String usuario) {
+        this.email = email;
+        this.password = password;
+        this.nombre = nombre;
+        this.usuario = usuario;
+    }
+
+    public User() {
+    }
+
+    
+
+
 }
