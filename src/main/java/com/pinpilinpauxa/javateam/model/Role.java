3dfe1b549c7b39e0f69957dee12f4f38ff4c3b37
joseanuoc <101591617+joseanuoc@users.noreply.github.com>
2022-11-28 21:41:48 +0100

    ...

--- src/main/java/com/pinpilinpauxa/javateam/model/Role.java
+++ src/main/java/com/pinpilinpauxa/javateam/model/Role.java
@@ -11,7 +11,7 @@ public class Role {
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Long id;
         private String nombre;
-
+ //Añadimos la relación con la clase Usuario
         @OneToMany(cascade = CascadeType.ALL)
         @JoinColumn(name="rol_id")
         private Set<User> usuarios;
