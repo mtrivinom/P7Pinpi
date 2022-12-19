17eec194bb57cc592bfff35c1e4c2d9386dfa068
mtrivinom <mtrivinom@uoc.edu>
2022-12-19 06:54:50 +0100

    OAuth2

--- src/main/java/com/pinpilinpauxa/javateam/security/WebSecurityConfig.java
+++ src/main/java/com/pinpilinpauxa/javateam/security/WebSecurityConfig.java
@@ -1,9 +1,11 @@
 package com.pinpilinpauxa.javateam.security;
 
+import com.pinpilinpauxa.javateam.rest.UserRestController;
 import com.pinpilinpauxa.javateam.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,8 +13,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 import javax.sql.DataSource;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -100,9 +106,40 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin();*/
 
-       //Código original del main
-
+        //Código original del main
 
 
     }
+
+    public class GitHub extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            // @formatter:off
+            http
+                    .authorizeRequests(a -> a
+                            .antMatchers("/", "/error", "/webjars/**").permitAll()
+                            .anyRequest().authenticated()
+                    )
+                    .exceptionHandling(e -> e
+                            .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                    )
+                    .oauth2Login();
+            // @formatter:on
+        }
+    }
+
+    public class OAuth2LoginSecurityConfig {
+
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+            http
+                    .authorizeHttpRequests(authorize -> authorize
+                            .anyRequest().authenticated()
+                    )
+                    .oauth2Login(withDefaults());
+            return http.build();
+        }
+
+    }
+
 }
